fa67bb41c93273104a63eedeeb18226ff73a51ef
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-10-14 14:25:53 +0530

    input from command line

--- node3/app3.js
+++ node3/app3.js
@@ -2,14 +2,41 @@
 //current data of the location.
 const geocode = require('./utils/geocode1');
 const forecast = require('./utils/forecast1');
+/** */
+const address = process.argv[2];
 
-const address = 'Halisahar';
-geocode(address,(error,data)=>{
-    const lat = data.long;
-    const long = data.lat;
-    forecast(lat,long,(error,data1)=>{
-        console.log("Real temparature is :",data1.temp);
-        console.log('Feellike :',data1.feel_temp);
-        console.log('Prediction is :',data1.des);
+if(!address){
+    console.log('Please provide an address');
+}
+else{
+
+ geocode(address,(error,data1)=>{
+    const lat = data1.long;
+    const long = data1.lat;
+    forecast(lat,long,(error,data2)=>{
+        console.log("Real temparature is :",data2.temp);
+        console.log('Feellike :',data2.feel_temp);
+        console.log('Prediction is :',data2.des);
+        console.log("Location :",data1.location);
     })
 });
+}
+/*
+//chaining callback
+geocode('Kolkata',(error,data1)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        forecast(data1.lat,data1.long,(error,data2)=>{
+            if(error){
+                console.log(error);
+            }
+            else{
+                console.log(data2.temp,data2.feel_temp,data2.des);
+                console.log(data1.location);
+            }
+        });
+    }
+});
+*/

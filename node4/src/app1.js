1114fabbab1f62d7863c54fffa6aa2d06f0942e2
Abhishek Gharami <abhishekgharami1998@gmail.com>
2022-10-18 11:11:59 +0530

    handlebars

--- node4/src/app1.js
+++ node4/src/app1.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs'); 
 
 
+ 
+//serving static src(javascript,html,css)
 const app = express();
 const public_path = path.join(__dirname,'../public');
+const partialpath = path.join(__dirname,'../src/partials');
 app.use(express.static(public_path));
+///serving dynamic src(javascript,html,css)
+app.set('view engine','hbs');
 
+// if you want to set costomized views then use
+//pathview = path.join(__dirname,'../folder_name');
+//use set method for setting the value
+//app.set('views',pathview)
+hbs.registerPartials(partialpath);
+app.get('',(req,res)=>{
+  res.render('index',{
+    title: "weather app",
+    name : "Abhishek Gharami"
+  })
+});
 
+//fixing about page
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        name : "Mr x",
+        title : "Mr y"
+    })
+}); 
 app.get('/help',(req,res)=>{
-    res.send('Help channel')
+    res.render('help',{
+     title : 'help',
+     name : 'Vishal'
+    })
 });
 
 app.get('/weather',(req,res)=>{
